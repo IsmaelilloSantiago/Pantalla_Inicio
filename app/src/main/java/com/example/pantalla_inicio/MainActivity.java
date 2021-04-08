733cafc56ae03dedddf277532ce9138128010804
IsmaelilloSantiago <ismael.santiago@elcalero.com>
2021-04-08 17:03:01 +0100

    Pantalla inicial e islas realizadas

---
+++ app/src/main/java/com/example/pantalla_inicio/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.pantalla_inicio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button boton_comenzar;
+    private TextView titulo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_islas);
+
+
+
+    }
+}
